08a97b687fa4dafbf0d75fabc247d384beb211dd
Dadamuga Mazino <dadamuga.mazino@zencargo.com>
2021-10-23 23:47:15 +0100

    Implemented URL Validation

--- api/src/resolvers/UrlResolver/UrlResolver.ts
+++ api/src/resolvers/UrlResolver/UrlResolver.ts
@@ -4,19 +4,26 @@ import { DeepPartial } from "typeorm";
 import { GenerateShortUrlInput } from "./types";
 import { customAlphabet } from "nanoid";
 import { lowercase, numbers } from "nanoid-dictionary";
+import { isWebUri } from "valid-url";
+import { UserInputError } from "apollo-server-errors";
 
 @Resolver()
 export class UrlResolver {
   @Mutation(() => Url)
   async generateShortUrl(
-    @Arg("input", () => GenerateShortUrlInput) input: GenerateShortUrlInput
+    @Arg("input", () => GenerateShortUrlInput)
+    { longUrl }: GenerateShortUrlInput
   ) {
+    if (!isWebUri(longUrl)) {
+      throw new UserInputError(`${longUrl} is not a valid URL.`);
+    }
+
     const dictionary: string = lowercase + numbers;
     const shortUuid: string = customAlphabet(dictionary, 8)();
     const shortUrl: string = `${process.env.SHORTENER_DOMAIN}/${shortUuid}`;
 
     const url: DeepPartial<Url> = {
-      longUrl: input.longUrl,
+      longUrl,
       shortUrl,
     };
 
