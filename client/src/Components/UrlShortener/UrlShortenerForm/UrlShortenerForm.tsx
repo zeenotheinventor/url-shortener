c989c6c12807b2aac08fdb48d1a0d0dddf1838e5
Dadamuga Mazino <destructicus_lives@yahoo.com>
2021-10-26 00:50:45 +0100

    Implemented UrlShortenerForm Logic

--- client/src/Components/UrlShortener/UrlShortenerForm/UrlShortenerForm.tsx
+++ client/src/Components/UrlShortener/UrlShortenerForm/UrlShortenerForm.tsx
@@ -3,6 +3,8 @@ import Button from "../../Button";
 import Input from "../../Input";
 import { Formik, Form } from "formik";
 import type { UrlShortenerFormValues } from "./types";
+import { isWebUri } from "valid-url";
+import { toast } from "react-toastify";
 
 interface Props {
   onSubmit: (longUrl: string) => void;
@@ -18,8 +20,28 @@ const UrlShortenerForm = ({ onSubmit }: Props): ReactElement => {
     form: { display: "flex" },
   };
 
+  const validate = ({ longUrl }: UrlShortenerFormValues) => {
+    const errors = [];
+
+    if (!longUrl) {
+      toast.error("Please enter a URL");
+      errors.push("longUrl");
+    } else if (!isWebUri(longUrl)) {
+      errors.push("longUrl");
+      toast.error("Invalid URL (must contain http domain)");
+    }
+
+    return errors;
+  };
+
   return (
-    <Formik initialValues={initialValues} onSubmit={handleClick}>
+    <Formik
+      initialValues={initialValues}
+      onSubmit={handleClick}
+      validateOnBlur={true}
+      validateOnChange={false}
+      validate={validate}
+    >
       {({ values, handleChange, handleSubmit }) => (
         <Form onSubmit={handleSubmit}>
           <div style={styles.form}>
