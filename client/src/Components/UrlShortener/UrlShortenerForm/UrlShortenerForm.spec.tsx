86355e6ae6a9e6d51fb15e75f3a3a1a4cec0edea
Dadamuga Mazino <destructicus_lives@yahoo.com>
2021-10-26 00:50:24 +0100

    Added UrlShortenerForm Tests

--- client/src/Components/UrlShortener/UrlShortenerForm/UrlShortenerForm.spec.tsx
+++ client/src/Components/UrlShortener/UrlShortenerForm/UrlShortenerForm.spec.tsx
@@ -1,14 +1,12 @@
 import "@testing-library/jest-dom/extend-expect";
 import "regenerator-runtime/runtime";
 import React from "react";
-import { render } from "@testing-library/react";
+import { fireEvent, render, waitFor } from "@testing-library/react";
 import UrlShortenerForm from ".";
 
 describe("UrlShortenerForm", () => {
   const subject = (propOverrides = {}) => {
-    return render(
-      <UrlShortenerForm onSubmit={(longUrl: string) => {}} {...propOverrides} />
-    );
+    return render(<UrlShortenerForm onSubmit={(longUrl: string) => {}} {...propOverrides} />);
   };
   it("renders form", () => {
     const { getByTestId } = subject();
@@ -18,5 +16,43 @@ describe("UrlShortenerForm", () => {
 
     expect(submitButton).toBeDefined();
     expect(inputField).toBeDefined();
+  });
+
+  describe("when url is valid", () => {
+    it("submits form on click", async () => {
+      const onSubmit = jest.fn();
+      const { getByTestId } = subject({ onSubmit });
+
+      const input: HTMLElement = getByTestId("url-field");
+      const submitButton: HTMLElement = getByTestId("atomify-button");
+
+      const validValue: string = "https://www.primarybid.com";
+
+      fireEvent.change(input, { target: { value: validValue } });
+      fireEvent.click(submitButton);
+
+      await waitFor(() => {
+        expect(onSubmit).toBeCalledWith(validValue);
+      });
+    });
+  });
+
+  describe("when url is invalid", () => {
+    it("does not submit form", async () => {
+      const onSubmit = jest.fn();
+      const { getByTestId } = subject({ onSubmit });
+
+      const input: HTMLElement = getByTestId("url-field");
+      const submitButton: HTMLElement = getByTestId("atomify-button");
+
+      const invalidValue: string = "www.primarybid.com";
+
+      fireEvent.change(input, { target: { value: invalidValue } });
+      fireEvent.click(submitButton);
+
+      await waitFor(() => {
+        expect(onSubmit).not.toBeCalled();
+      });
+    });
   });
 });
